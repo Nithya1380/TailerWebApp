cf02e2e1783de07a37aa2626ceece3fcfad62564
nithya <nithya.mach@gmail.com>
2018-10-08 23:22:48 +0530

    ch

--- DAL/DBManager/CustomerManager.cs
+++ DAL/DBManager/CustomerManager.cs
@@ -423,8 +423,7 @@ namespace DAL.DBManager
             return ret;
         }
 
-<<<<<<< HEAD
-=======
+
         public bool SaveCustomerDetails(int companyID, int userID,int customerID,int customerBranch,ref CustomerMaster customerObj,out int newCustomerID)
         {
             bool ret = false;
@@ -471,6 +470,5 @@ namespace DAL.DBManager
             }
             return ret;
         }
->>>>>>> c04d10f1d791808acc5b513b3a75a873a77f2865
     }
 }
